b3fb9f09dc59657cb6f3fb5c387999d08f81e881
janiev <jan.vandoorne@gmail.com>
2020-04-16 15:02:07 +0200

    Add files via upload

--- furthestPointFinder.m
+++ furthestPointFinder.m
@@ -1,9 +1,9 @@
-function value = furthestPointFinder(x)
+function value = furthestPointFinder(x,maxHeight)
 %Takes a binary  image as input. Function finds the furthest point !(y,x)! coordinates of Straw.
 binaryImage = x;
 
 furthestPoint = [0 0];
-for y = 330:760
+for y = maxHeight:655
     furthestPointRow = [0 0];
     for x = 330:940
         if and(binaryImage(y,x)==1,x > furthestPointRow(2))

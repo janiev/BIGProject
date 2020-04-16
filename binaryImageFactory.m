fe968bf377f584bca7cf95e77bce970d3055663d
janiev <jan.vandoorne@gmail.com>
2020-04-16 15:28:15 +0200

    Update binaryImageFactory.m

--- binaryImageFactory.m
+++ binaryImageFactory.m
@@ -4,8 +4,9 @@ function binaryImage = binaryImageFactory(jpgImage,threshholdValue)
 %remove lens distortion
 undistortedImage = undistordImage(jpgImage,cameraParams);
 
+image = histeq(undistortedImage);
 %convert to grayscale image
-image = rgb2gray(undistortedImage);
+image = rgb2gray(image);
 
 %kies de threshold value
 binaryImage = image < threshholdValue;

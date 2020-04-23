0c96e1e71581303b9b4254d1ac55d3cc795c2985
janiev <jan.vandoorne@gmail.com>
2020-04-23 20:49:11 +0200

    Add files via upload

--- plotPositionData.m
+++ plotPositionData.m
@@ -16,7 +16,7 @@ end
 plot(t,y);
 
 %%
-t = 1/frameRate:1/frameRate:0.1204;
+t = 1/FRAMERATE:1/FRAMERATE:(maxPhotos-minPhotos+1)/FRAMERATE;
 y = [];
 for i = 1:(maxPhotos-minPhotos+1)
     %y(i) = ((positionData{i}(1)-413)^2+(positionData{i}(2)-680)^2)^(1/2);

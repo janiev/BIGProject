6e0902f9f61cddf04b601502c29c841080a70b34
janiev <jan.vandoorne@gmail.com>
2020-04-24 13:40:33 +0200

    Add files via upload

--- calculateDamping.m
+++ calculateDamping.m
@@ -27,7 +27,7 @@ end
 %enhance topList ==> make toplistEnhanced
 topListDiffMean = mean(diff(topList));
 topListEnhanced = [];
-for i = 3:length(topList)/3
+for i = 3:length(topList)/4
     
     if abs(topList(i)-topList(i+1))>topListDiffMean*0.8
         topListEnhanced(end+1) = topList(i);

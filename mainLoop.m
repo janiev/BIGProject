45b38431998a0bd67011c79a2b495aec2a42cee9
janiev <jan.vandoorne@gmail.com>
2020-04-24 13:50:45 +0200

    Add files via upload

--- mainLoop.m
+++ mainLoop.m
@@ -1,10 +1,22 @@
+%DODO specify the picture folder
+
+pictureFolder = uigetdir('lmao');%'C:\Users\janva\Desktop\skool\2019_damping_wheat\20190712\images\stem_002broken.5313.258';
+pluk = [];
+for i = 1:length(pictureFolder)
+    if pictureFolder(i) == '.'
+        pluk(end+1)=i;
+    end
+end
+
+maxH = str2num(pictureFolder(pluk(2)+1:length(pictureFolder)));
+
 %DODO
-minPhotos = 4297;%input('the smallest index');
+minPhotos = str2num(pictureFolder(pluk(1)+1:pluk(2)-1));%1197;input('the smallest index');
 maxPhotos = minPhotos+300;%input('please the highest index of a photo in the dataset');
 
 %constnts
 FRAMERATE = 250;
-PIXELSIZE = 0.001;
+PIXELSIZE = 0.7143;
 
 %% Check of maxPhotos valid is
 %TO DO : geen check voor minPhotos, en enkel check of maxPhotos niet te groot is
@@ -19,10 +31,6 @@ end
 positionData = cell(1,maxPhotos-minPhotos+1);
 
 
-%DODO specify the picture folder
-pictureFolder = 'C:\Users\janva\Desktop\skool\2019_damping_wheat\20190823\images\stem_014';
-
-
 %generate averagePicture
 averagePicture = histeq(averagePictureFactory(pictureFolder,minPhotos,minPhotos+15));
 
@@ -30,6 +38,7 @@ for i = minPhotos:maxPhotos
 
 %Give user feedback program is running
 disp([num2str(((i-minPhotos+1)/(maxPhotos-minPhotos+1))*100) '%'])
+
 pictureRank = i-minPhotos+1;
 
 
@@ -45,8 +54,16 @@ binaryImage = bwareaopen(binaryImage,50);
 
 
 %DODO now proces image and find furthestpoint
-positionData{pictureRank} = furthestPointFinder(binaryImage,220);
+positionData{pictureRank} = furthestPointFinder(binaryImage,maxH);
 
 end
 %%
 pm = calculateDamping([0 0],[0 0],positionData,PIXELSIZE,FRAMERATE);
+
+t = 1/FRAMERATE:1/FRAMERATE:(maxPhotos-minPhotos+1)/FRAMERATE;
+y = [];
+for i = 1:(maxPhotos-minPhotos+1)
+    %y(i) = ((positionData{i}(1)-413)^2+(positionData{i}(2)-680)^2)^(1/2);
+    y(i) = positionData{i}(1)*PIXELSIZE;
+end
+plot(t,y);
